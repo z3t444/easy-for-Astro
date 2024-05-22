00ddf9dba03c3cf5fc46c660ad478cfd459d53d5
Zeeta:) <113794999+z3t444@users.noreply.github.com>
2024-05-22 03:44:39 -0700

    easy for Astro JS, Love it

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    './src/**/*.{astro,js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
